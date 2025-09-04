a2a28bab1148836ed29f038d5d7c694fb0532792
ISkyCraftI <matt.damien@icloud.com>
2025-09-04 15:44:45 +0200

    Com /singularity

--- quark/singularity/build.sh
+++ quark/singularity/build.sh
@@ -1,1 +1,6 @@
 # Script de build du conteneur Singularity pour Quark
+
+#!/bin/bash
+# singularity/build.sh - Script de construction du container
+
+## TODO: Ajouter les options de build nécessaire
--- quark/singularity/run.sh
+++ quark/singularity/run.sh
@@ -1,1 +1,6 @@
 # Script de lancement du conteneur Singularity pour Quark
+
+#!/bin/bash
+# singularity/run.sh - Script de lancement du container
+
+## TODO: Ajouter les options de run nécessaires
